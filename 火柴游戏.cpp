1625505a7b06ed9ca0b887a77b83613a299198aa
cao-xiang <56731499+cao-xiang@users.noreply.github.com>
2019-11-05 22:51:18 +0800

    Add files via upload

---
+++ 火柴游戏.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main ()
+ {
+ 	int  n,t;
+ 	int a=1,b=0; 
+ 	while(scanf("%d",&n))
+ 	{
+	  if(n%3==0)
+	 {
+	 	printf("%d\n",b);
+	  }
+	  else
+	  {
+	  	printf("%d\n",a);
+	  }
+     }
+	  return 0;
+ }
