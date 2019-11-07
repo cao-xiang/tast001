337902ee903947f29a230058a5765125511b0487
cao-xiang <56731499+cao-xiang@users.noreply.github.com>
2019-11-07 22:15:20 +0800

    Add files via upload

---
+++ 计算两点间的距离.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	double x1,y1,x2,y2,A;
+	double result; 	
+	while(~scanf("%lf %lf %lf %lf",&x1,&y1,&x2,&y2)!=NULL) 
+	{
+    A=(x1-x2)*(x1-x2)+(y1-y2)*(y1-y2);
+	result=sqrt(A);
+	printf("%.2lf\n",result);
+	}
+ } 
