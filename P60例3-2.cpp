e751090a5aee98188e261c9933a9456d9da2b83d
cao-xiang <56731499+cao-xiang@users.noreply.github.com>
2019-11-27 21:49:17 +0800

    Add files via upload

---
+++ P60例3-2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+	printf("%c"£¬g);
+	printf("%c"£¬65);
+	printf("%d"£¬-35);
+	printf("%o"£¬35);
+	printf("%u"£¬35);
+	printf("%X"£¬35);
+	printf("%ld\n"£¬-23L);
+	printf("%f"£¬10.2345678);
+	printf("%E"£¬10.2345678);
+	printf("%f"£¬10.2345678);
+	printf("%lE\n"£¬10.2345678);
+	printf("%s"£¬"hello C");
+}
